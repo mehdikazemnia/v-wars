43f0544327aa0df7125fa1d3e32b4cea63067856
mahdikazemnia <mahdikazemnia@users.noreply.github.com>
2017-11-11 20:25:00 +0330

    webpack comes

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: './public/js/entry.js',
+    watch: true,
+    output: {
+        filename: './public/js/bundle.js',
+        sourceMapFilename: './public/js/bundle.js.map'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: { presets: ['es2015', 'react'] }
+        }]
+    }
+};
