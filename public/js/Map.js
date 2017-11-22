a1af9054f124cea5e99b189cb5dea63cd1fddc95
mahdikazemnia <mahdikazemnia@users.noreply.github.com>
2017-11-22 12:53:10 +0330

    better map for tests

--- public/js/Map.js
+++ public/js/Map.js
@@ -11,75 +11,195 @@ const Map = {
             color: '#aa0000'
         },
         {
-            current : true,
+            current: true,
             id: 'p22222',
             name: 'akbar',
             color: '#009900'
         }
     ],
     cells: [{
-            x: 20,
+            x: 50,
             y: 50,
-            capacity: 100,
-            population: 1,
-            playerid: 'p11111'
+            capacity: 150,
+            population: 20,
+            playerid: false
         },
         {
-            x: 35,
-            y: 60,
-            capacity: 50,
-            population: 1,
-            playerid: false            
-        },
-        {
-            x: 35,
+            x: 10,
             y: 30,
             capacity: 50,
             population: 1,
-            playerid: false            
+            playerid: false
         },
         {
-            x: 55,
-            y: 50,
-            capacity: 150,
+            x: 20,
+            y: 20,
+            capacity: 50,
             population: 1,
-            playerid: 'p22222' 
+            playerid: false
+        },
+        {
+            x: 30,
+            y: 15,
+            capacity: 50,
+            population: 1,
+            playerid: false
+        },
+        {
+            x: 40,
+            y: 15,
+            capacity: 50,
+            population: 1,
+            playerid: false
+        },
+        {
+            x: 50,
+            y: 20,
+            capacity: 50,
+            population: 1,
+            playerid: false
+        },
+        {
+            x: 60,
+            y: 15,
+            capacity: 50,
+            population: 1,
+            playerid: false
+        },
+        {
+            x: 70,
+            y: 15,
+            capacity: 50,
+            population: 1,
+            playerid: false
         },
         {
             x: 80,
-            y: 50,
-            capacity: 200,
+            y: 20,
+            capacity: 50,
             population: 1,
-            playerid: 'p22222' 
+            playerid: false
+        },
+        {
+            x: 90,
+            y: 30,
+            capacity: 50,
+            population: 1,
+            playerid: false
         },
         {
             x: 10,
             y: 70,
             capacity: 50,
             population: 1,
-            playerid: false            
-        },
-        {
-            x: 10,
-            y: 35,
-            capacity: 50,
-            population: 1,
-            playerid: false            
+            playerid: false
         },
         {
             x: 20,
-            y: 85,
-            capacity: 50,
-            population: 1,
-            playerid: false            
-        },
-        {
-            x: 75,
             y: 80,
             capacity: 50,
             population: 1,
-            playerid: false            
+            playerid: false
         },
+        {
+            x: 30,
+            y: 85,
+            capacity: 50,
+            population: 1,
+            playerid: false
+        },
+        {
+            x: 40,
+            y: 85,
+            capacity: 50,
+            population: 1,
+            playerid: false
+        },
+        {
+            x: 50,
+            y: 80,
+            capacity: 50,
+            population: 1,
+            playerid: false
+        },
+        {
+            x: 60,
+            y: 85,
+            capacity: 50,
+            population: 1,
+            playerid: false
+        },
+        {
+            x: 70,
+            y: 85,
+            capacity: 50,
+            population: 1,
+            playerid: false
+        },
+        {
+            x: 80,
+            y: 80,
+            capacity: 50,
+            population: 1,
+            playerid: false
+        },
+        {
+            x: 90,
+            y: 70,
+            capacity: 50,
+            population: 1,
+            playerid: false
+        },
+        {
+            x: 80,
+            y: 50,
+            capacity: 200,
+            population: 1,
+            playerid: 'p11111'
+        },
+        {
+            x: 65,
+            y: 35,
+            capacity: 100,
+            population: 1,
+            playerid: 'p11111'
+        },
+        {
+            x: 65,
+            y: 65,
+            capacity: 100,
+            population: 1,
+            playerid: 'p11111'
+        },
+        {
+            x: 20,
+            y: 50,
+            capacity: 200,
+            population: 1,
+            playerid: 'p22222'
+        },
+        {
+            x: 35,
+            y: 35,
+            capacity: 100,
+            population: 1,
+            playerid: 'p22222'
+        },
+        {
+            x: 35,
+            y: 65,
+            capacity: 100,
+            population: 1,
+            playerid: 'p22222'
+        },
+
+
+
+
+
+
+
+
     ]
 }
-export default Map 
+export default Map
