ce8987bb7eda20ffbb50b54a422752412cd45772
mahdikazemnia <mahdikazemnia@users.noreply.github.com>
2017-11-27 23:39:43 +0330

    different repultion forces by cells radius (mass)

--- public/js/Cell.js
+++ public/js/Cell.js
@@ -21,6 +21,7 @@ class Cell {
         this.scale = (this.capacity > 150) ? 200 : (this.capacity > 100) ? 150 : (this.capacity > 50) ? 100 : (this.capacity > 0) ? 50 : null;
         this.scale = this.scale / 200
         this.r = 100 * this.scale
+        this.mass = Math.round(this.r * this.r * Math.PI * .0005)
 
         // appending viruses
         for (let i = 0; i < info.population; i++) {
@@ -31,7 +32,7 @@ class Cell {
         // gravity and repultion
         this.gravity = 10
         this.repultion = {
-            k: 7000, // amount of power then repulsing     
+            k: 500, // amount of power then repulsing     
             margin: this.r * 1.2
         }
 
@@ -210,7 +211,7 @@ class Cell {
             let distancep = Math.sqrt((dxp * dxp) + (dyp * dyp))
             // *****************
 
-            let F = this.repultion.k / (distancep * distancep)
+            let F = this.repultion.k * this.mass / (distancep * distancep)
 
             let force = {
                 dx: (dxp / distancep) * F,
