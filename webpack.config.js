889d23f66818337970b7da5e529860dcec454aa2
mehdikazemnia <mehdikazemnia1998@gmail.com>
2019-12-31 15:19:08 +0330

    to hell with babel, mess cleaned up

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-
 module.exports = {
     devtool: 'inline-source-map',
     entry: './public/js/entry.js',
@@ -7,15 +5,5 @@ module.exports = {
     output: {
         filename: './public/js/bundle.js',
         sourceMapFilename: './public/js/bundle.js.map'
-    },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        }]
-    },
-    plugins: [
-       // new UglifyJsPlugin()
-    ]
+    }
 };
